81df861f6a5d11f7c2c6468d28e304e2e3ad79f7
Kok357357 <huskar357357@gmail.com>
2020-04-14 01:01:18 +0800

    finished web version

--- app/scripts/academia/controllers.js
+++ app/scripts/academia/controllers.js
@@ -226,122 +226,119 @@ angular.module('netbase')
                 }
             }
         }
-        if(!$scope.isMobile()) {
-          console.log('this is not mobile device');
-          mainWidth = parseInt(videoContainer.offsetWidth);
-          videoContainer.style.height = mainWidth / 4 * 3 + 'px';
-          mainHeight = mainWidth / 4 * 3;
-          //mainWidth -= 100;
-          //mainHeight -= 75;
-          let px = 'px';
-          if (titleDom.length == 1 || countOfNone == 1) {
-              titleDom[0].style.width = "100%";
-              titleDom[0].style.height = "100%";
-              //titleDom[0].style.left = '50px';
-              //titleDom[0].style.top = '37.5px';
-              let k;
-              for (k = 0; k < titleDom[0].childElementCount; k++) {
-                  if (titleDom[0].children[k].tagName == 'VIDEO') {
-  
-                      titleDom[0].children[k].style.width = "100%";
-                      titleDom[0].children[k].style.height = "100%";
-                  }
-              }
-  
-              showingTitle.style.width = "100%";
-              showingTitle.style.height = "100%";
-              for (k = 0; k < showingTitle.childElementCount; k++) {
-                  if (showingTitle.children[k].tagName == 'VIDEO') {
-  
-                      showingTitle.children[k].style.width = "100%";
-                      showingTitle.children[k].style.height = "100%";
-                  }
-              }
-              //showingTitle.style.left = '50px';
-              //showingTitle.style.top = '37.5px';
-  
-          } else if (videoDom.length > 1 && videoDom.length < 5 && countOfNone != 1) {
-              for (i = 0; i < titleDom.length; i += 1) {
-                  let k;
-                  for (k = 0; k < titleDom[i].childElementCount; k++) {
-                      if (titleDom[i].children[k].tagName == 'VIDEO') {
-                          titleDom[i].children[k].style.width = "100%";
-                          titleDom[i].children[k].style.height = "100%";
-                      }
-                  }
-                  titleDom[i].style.width = "50%";
-                  titleDom[i].style.height = "50%";
-              }
-          } 
-          else if (videoDom.length > 4 && countOfNone != 1) {
-            for (i = 0; i < titleDom.length; i += 1) {
+        if (!$scope.isMobile()) {
+            console.log('this is not mobile device');
+            mainWidth = parseInt(videoContainer.offsetWidth);
+            videoContainer.style.height = mainWidth / 4 * 3 + 'px';
+            mainHeight = mainWidth / 4 * 3;
+            //mainWidth -= 100;
+            //mainHeight -= 75;
+            let px = 'px';
+            if (titleDom.length == 1 || countOfNone == 1) {
+                titleDom[0].style.width = "100%";
+                titleDom[0].style.height = "100%";
+                //titleDom[0].style.left = '50px';
+                //titleDom[0].style.top = '37.5px';
                 let k;
-                for (k = 0; k < titleDom[i].childElementCount; k++) {
-                    if (titleDom[i].children[k].tagName == 'VIDEO') {
-                        titleDom[i].children[k].style.width = "100%";
-                        titleDom[i].children[k].style.height = "100%";
+                for (k = 0; k < titleDom[0].childElementCount; k++) {
+                    if (titleDom[0].children[k].tagName == 'VIDEO') {
+
+                        titleDom[0].children[k].style.width = "100%";
+                        titleDom[0].children[k].style.height = "100%";
                     }
                 }
-                titleDom[i].style.width = "33.333%";
-                titleDom[i].style.height = "33.333%";
-            }
-          }
-        }
-        else if ($scope.isMobile()) {
-          console.log("this is a mobile device");
-          videoContainer.style.height = parseInt(screen.height) - 102 + 'px';
-          if (titleDom.length == 1 || countOfNone == 1) {
-            titleDom[0].style.width = "100%";
-            titleDom[0].style.height = "100%";
-            //titleDom[0].style.left = '50px';
-            //titleDom[0].style.top = '37.5px';
-            let k;
-            for (k = 0; k < titleDom[0].childElementCount; k++) {
-                if (titleDom[0].children[k].tagName == 'VIDEO') {
 
-                    titleDom[0].children[k].style.width = "100%";
-                    titleDom[0].children[k].style.height = "100%";
-                }
-            }
+                showingTitle.style.width = "100%";
+                showingTitle.style.height = "100%";
+                for (k = 0; k < showingTitle.childElementCount; k++) {
+                    if (showingTitle.children[k].tagName == 'VIDEO') {
 
-            showingTitle.style.width = "100%";
-            showingTitle.style.height = "100%";
-            for (k = 0; k < showingTitle.childElementCount; k++) {
-                if (showingTitle.children[k].tagName == 'VIDEO') {
-
-                    showingTitle.children[k].style.width = "100%";
-                    showingTitle.children[k].style.height = "100%";
-                }
-            }
-            //showingTitle.style.left = '50px';
-            //showingTitle.style.top = '37.5px';
-
-          } else if (videoDom.length ==2 && countOfNone != 1) {
-              for (i = 0; i < titleDom.length; i += 1) {
-                  let k;
-                  for (k = 0; k < titleDom[i].childElementCount; k++) {
-                      if (titleDom[i].children[k].tagName == 'VIDEO') {
-                          titleDom[i].children[k].style.width = "100%";
-                          titleDom[i].children[k].style.height = "100%";
-                      }
-                  }
-                  titleDom[i].style.width = "100%";
-                  titleDom[i].style.height = "50%";
-              }
-          } 
-          else if (videoDom.length > 2 && countOfNone != 1) {
-            for (i = 0; i < titleDom.length; i += 1) {
-                let k;
-                for (k = 0; k < titleDom[i].childElementCount; k++) {
-                    if (titleDom[i].children[k].tagName == 'VIDEO') {
-                        titleDom[i].children[k].style.width = "100%";
-                        titleDom[i].children[k].style.height = "100%";
+                        showingTitle.children[k].style.width = "100%";
+                        showingTitle.children[k].style.height = "100%";
                     }
                 }
-                titleDom[i].style.width = "50%";
-                titleDom[i].style.height = "50%";
+                //showingTitle.style.left = '50px';
+                //showingTitle.style.top = '37.5px';
+
+            } else if (videoDom.length > 1 && videoDom.length < 5 && countOfNone != 1) {
+                for (i = 0; i < titleDom.length; i += 1) {
+                    let k;
+                    for (k = 0; k < titleDom[i].childElementCount; k++) {
+                        if (titleDom[i].children[k].tagName == 'VIDEO') {
+                            titleDom[i].children[k].style.width = "100%";
+                            titleDom[i].children[k].style.height = "100%";
+                        }
+                    }
+                    titleDom[i].style.width = "50%";
+                    titleDom[i].style.height = "50%";
+                }
+            } else if (videoDom.length > 4 && countOfNone != 1) {
+                for (i = 0; i < titleDom.length; i += 1) {
+                    let k;
+                    for (k = 0; k < titleDom[i].childElementCount; k++) {
+                        if (titleDom[i].children[k].tagName == 'VIDEO') {
+                            titleDom[i].children[k].style.width = "100%";
+                            titleDom[i].children[k].style.height = "100%";
+                        }
+                    }
+                    titleDom[i].style.width = "33.333%";
+                    titleDom[i].style.height = "33.333%";
+                }
             }
-          }
+        } else if ($scope.isMobile()) {
+            console.log("this is a mobile device");
+            videoContainer.style.height = parseInt(screen.height) - 102 + 'px';
+            if (titleDom.length == 1 || countOfNone == 1) {
+                titleDom[0].style.width = "100%";
+                titleDom[0].style.height = "100%";
+                //titleDom[0].style.left = '50px';
+                //titleDom[0].style.top = '37.5px';
+                let k;
+                for (k = 0; k < titleDom[0].childElementCount; k++) {
+                    if (titleDom[0].children[k].tagName == 'VIDEO') {
+
+                        titleDom[0].children[k].style.width = "100%";
+                        titleDom[0].children[k].style.height = "100%";
+                    }
+                }
+
+                showingTitle.style.width = "100%";
+                showingTitle.style.height = "100%";
+                for (k = 0; k < showingTitle.childElementCount; k++) {
+                    if (showingTitle.children[k].tagName == 'VIDEO') {
+
+                        showingTitle.children[k].style.width = "100%";
+                        showingTitle.children[k].style.height = "100%";
+                    }
+                }
+                //showingTitle.style.left = '50px';
+                //showingTitle.style.top = '37.5px';
+
+            } else if (videoDom.length == 2 && countOfNone != 1) {
+                for (i = 0; i < titleDom.length; i += 1) {
+                    let k;
+                    for (k = 0; k < titleDom[i].childElementCount; k++) {
+                        if (titleDom[i].children[k].tagName == 'VIDEO') {
+                            titleDom[i].children[k].style.width = "100%";
+                            titleDom[i].children[k].style.height = "100%";
+                        }
+                    }
+                    titleDom[i].style.width = "100%";
+                    titleDom[i].style.height = "50%";
+                }
+            } else if (videoDom.length > 2 && countOfNone != 1) {
+                for (i = 0; i < titleDom.length; i += 1) {
+                    let k;
+                    for (k = 0; k < titleDom[i].childElementCount; k++) {
+                        if (titleDom[i].children[k].tagName == 'VIDEO') {
+                            titleDom[i].children[k].style.width = "100%";
+                            titleDom[i].children[k].style.height = "100%";
+                        }
+                    }
+                    titleDom[i].style.width = "50%";
+                    titleDom[i].style.height = "50%";
+                }
+            }
         }
     }
 
@@ -363,7 +360,7 @@ angular.module('netbase')
     $scope.isMobile = function() {
         var check = false;
         (function(a) { if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(a) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0, 4))) check = true; })(navigator.userAgent || navigator.vendor || window.opera);
-        if(screen.width < 500) check = true;
+        if (screen.width < 500) check = true;
         return check;
     }
 
@@ -455,7 +452,7 @@ angular.module('netbase')
                 }
                 if (res.data.data._id == $scope.administrator[0]._id) {
                     $scope.adminActive = 'admin-active';
-                } else {        
+                } else {
                     $scope.showingParticipants.push(res.data.data);
                 }
                 $scope.participants.push(res.data.data);
@@ -692,21 +689,19 @@ angular.module('netbase')
 
     $scope.toggleParticipantsBox = function() {
         $scope.participantsStatus = !$scope.participantsStatus;
-        if($scope.participantsStatus){
-          $scope.mobileToggleParticipantsList = 'drag-in-left-right';
-        }
-        else {
-          $scope.mobileToggleParticipantsList = 'drag-out-right-left';
+        if ($scope.participantsStatus) {
+            $scope.mobileToggleParticipantsList = 'drag-in-left-right';
+        } else {
+            $scope.mobileToggleParticipantsList = 'drag-out-right-left';
         }
     }
 
     $scope.toggleChatBox = function() {
         $scope.chatStatus = !$scope.chatStatus;
-        if($scope.chatStatus){
-          $scope.chatboxContainer = "drag-in-right-left";
-        }
-        else{
-          $scope.chatboxContainer = "drag-out-left-right";
+        if ($scope.chatStatus) {
+            $scope.chatboxContainer = "drag-in-right-left";
+        } else {
+            $scope.chatboxContainer = "drag-out-left-right";
         }
     }
 
@@ -760,12 +755,11 @@ angular.module('netbase')
     }
 
     $scope.toggleAllControllers = function() {
-      if($scope.mobileVisibleToggle == 'mobile-invisible'){
-        $scope.mobileVisibleToggle = 'mobile-visible';
-      }
-      else {
-        $scope.mobileVisibleToggle = 'mobile-invisible';
-      }
+        if ($scope.mobileVisibleToggle == 'mobile-invisible') {
+            $scope.mobileVisibleToggle = 'mobile-visible';
+        } else {
+            $scope.mobileVisibleToggle = 'mobile-invisible';
+        }
     }
 }])
 
@@ -792,8 +786,6 @@ angular.module('netbase')
     console.log('here local video');
     console.log(localVideo);
 
-    $scope.classroomViewMode = false;
-
     //var baseUrl = "http://localhost:9000"; //Back-end server base url
     var baseUrl = "http://localhost:9001"; //Back-end server base url
     var baseUrl = "https://educationalcommunity-classroom.herokuapp.com";
@@ -814,23 +806,8 @@ angular.module('netbase')
         //$scope.isMobile();
     });
 
-    /****************** Mobile / Web **************************/
-
-    $scope.isMobile = function() {
-        var check = false;
-        (function(a) { if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(a) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0, 4))) check = true; })(navigator.userAgent || navigator.vendor || window.opera);
-        $scope.classroomViewMode = check;
-        return check;
-    }
-
-    // $scope.maxHeight = function() {
-    //     if ($scope.classroomViewMode == true)
-    //         return { "max-height": '500px' }
-    //     else return { 'max-height': '600px' }
-    // }
-
     $scope.maxHeight = {
-        "max-height": '500px'
+        "max-height": '600px'
     }
 
     /******************** GET ALL Classrooms ******************/
