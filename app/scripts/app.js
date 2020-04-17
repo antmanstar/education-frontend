53a336f6a919de3a13534727f69277af65fd03b3
Marcelo Pio <marcelopiomsf@gmail.com>
2020-04-17 12:42:50 -0300

    Revert "i have changes in university timeline and user timeline"

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -19,10 +19,8 @@ angular.module('netbase', [
     'ngSanitize',
     'infinite-scroll',
     'updateMeta',
-    'as.sortable',
-    'oc.lazyLoad'
+    'as.sortable'
 ])
-
 .config(['$translateProvider', '$localStorageProvider', 'StripeElementsProvider', function ($translateProvider, $localStorageProvider, StripeElementsProvider) {
 
   //let stripeKey = "pk_live_ZBmOf7GNQ13AIEGeP9WkPv3M";
@@ -55,7 +53,6 @@ angular.module('netbase', [
   });
 
   $translateProvider.preferredLanguage('en');
-
 
 }])
 
@@ -131,11 +128,6 @@ angular.module('netbase', [
         .when('/cursos/id/:id/timeline', {
             templateUrl: 'partials/courses/dashboard/index.html',
             controller: 'CoursesByIdDashboardCtrl',
-            resolve: {
-                LazyLoadCtrl: ['$ocLazyLoad', function($ocLazyLoad) {
-                    return $ocLazyLoad.load('CourseTimelineController'); // Resolve promise and load before view 
-                }]
-            },
         })
         .when('/cursos/id/:id/modulos', {
             templateUrl: 'partials/courses/dashboard/modulo.html',
@@ -244,11 +236,6 @@ angular.module('netbase', [
         .when('/a/:academiaName/timeline', {
             templateUrl: 'partials/academia/academiatimeline.html',
             controller: 'AcademiaTimelineCtrl',
-            resolve: {
-                LazyLoadCtrl: ['$ocLazyLoad', function($ocLazyLoad) {
-                    return $ocLazyLoad.load('UniversityTimelineController'); // Resolve promise and load before view 
-                }]
-            },
         })
         .when('/a/:academiaName/playlist/all', {
             templateUrl: 'partials/academia/academiaplaylist.html',
@@ -477,12 +464,7 @@ angular.module('netbase', [
         .when('/home/timeline', {
             templateUrl: 'partials/home/hometimeline.html',
             controller: 'HomeTimelineCtrl',
-            resolve: {
-                auth: auth.app,
-                LazyLoadCtrl: ['$ocLazyLoad', function($ocLazyLoad) {
-                    return $ocLazyLoad.load('HomeTimelineController'); // Resolve promise and load before view 
-                }]
-             },
+            resolve: auth
         })
         .when('/home/noticias', {
             templateUrl: 'partials/home/homenews.html',
