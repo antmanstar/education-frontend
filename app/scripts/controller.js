95161b8c4334b6d66f07af66dceaf8eea4c972f0
Marcelo Pio <marcelopiomsf@gmail.com>
2020-09-23 07:05:13 -0300

    whitelabel HeaderCtrl implementation

--- app/scripts/controller.js
+++ app/scripts/controller.js
@@ -5595,10 +5595,12 @@ angular.module('netbase')
 
 /* end messenger */
 
-.controller('HeaderCtrl', ['$rootScope', '$scope', '$location', '$localStorage', 'jwtHelper', 'Search', 'Students', '$route', 'ngDialog', '$timeout', 'Chat', '$translate', function($rootScope, $scope, $location, $localStorage, jwtHelper, Search, Students, $route, ngDialog, $timeout, Chat, $translate) {
+.controller('HeaderCtrl', ['$rootScope', '$scope', '$location', '$localStorage', 'jwtHelper', 'Search', 'Students', '$route', 'ngDialog', '$timeout', 'Chat', '$translate', '$routeParams', 'University', function($rootScope, $scope, $location, $localStorage, jwtHelper, Search, Students, $route, ngDialog, $timeout, Chat, $translate, $routeParams, University) {
 
     /* header variables */
     let logged = $rootScope.logged;
+
+    $scope.whitelabel = false;
 
     $scope.searchBarDisplay = false;
 
@@ -5607,13 +5609,66 @@ angular.module('netbase')
 
     /* function that change the language */
     $scope.changeLanguage = function(langKey) {
-        $translate.use(langKey);
+      $translate.use(langKey);
 
-        // set selected language to localstorage
-        $localStorage.setLanguage = langKey
-        $scope.selectedLanguage = langKey
+      // set selected language to localstorage
+      $localStorage.setLanguage = langKey
+      $scope.selectedLanguage = langKey
     };
     /**/
+
+    // start - WHITELABEL IMPLEMENTATION
+
+
+    // #1 = If ?wl=1, then
+
+    if ($location.search().wl == 1) {
+
+      console.log("IS WHITELABELLLLLLL REQUEST!!!!!!!!!!!!!!!!!!!")
+
+      var universityUrl = $routeParams.academiaName;
+      console.log("university url: ")
+      console.log(universityUrl)
+
+      // #2.1 = Load University
+
+      if (University.isStoredLocal(universityUrl)) {
+
+        // #2 =  replace “Universidade”/”College”/”Universidad” logo for University custom logo
+        // #3 = Remove Universidades and Explorar from Tab
+        let universityStorage = University.retrieveStorage(universityUrl);
+        $scope.university = universityStorage[universityUrl];
+        if ($scope.university.whitelabel == true) {
+          $scope.whitelabel = true;
+        }
+        console.log("universit111111111111111y: ")
+        console.log($scope.university)
+
+      } else {
+
+        // #2 =  replace “Universidade”/”College”/”Universidad” logo for University custom logo
+        // #3 = Remove Universidades and Explorar from Tab
+
+          University.getUniversity(universityUrl).then(function(res) {
+
+              console.log("universit22222222222222y: ")
+              console.log(res.data.data)
+              $scope.university = res.data.data;
+              if ($scope.university.whitelabel == true) {
+                $scope.whitelabel = true;
+              }
+              University.storeLocal($scope.university);
+
+
+          });
+          //
+
+      }
+
+
+    }
+
+    // end - WHITELABEL IMPLEMENTATION
 
     /* functions */
 
